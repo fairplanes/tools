c20a4c70bb2b43b68d13ca24d759a337f9890e9b
fairplanes <walkinggfuelbillboard@gmail.com>
2025-04-21 13:30:31 -0500

    Update forlists.py

--- forlists.py
+++ forlists.py
@@ -3,3 +3,4 @@ words = input("Enter your words here: ")
 def wordsfinale():
     return words.replace(" ","','")
 print(wordsfinale())
+#Add a space at the beginning and end of your list. Then, just highlight and copy what you need.
